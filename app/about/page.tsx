840fb401b8b0af16cbdd1cf4408e3636564ec5e7
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-11-13 21:28:37 -0800

    test

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  return (
+    <main className="p-8">
+      <h1>About Me</h1>
+       <p className="mt-4">
+        I’m a software engineer passionate about building creative, human-centered apps.
+      </p>
+    </main>
+  );
+}
